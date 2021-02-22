3b1edff1d2480415bcaca248edd2acaa18ca7422
CodeAllen <allen5g@qq.com>
2021-02-22 22:39:26 +0800

    Update 001字符串最后一个单词的长度.c

--- 001字符串最后一个单词的长度.c
+++ 001字符串最后一个单词的长度.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 #include<string.h>
-#include<windows.h>
+
 int main()
 {
     char str[128];
-    gets(str);
+    gets(str);    //从屏幕获取字符串
     int count=0;
-    int len=strlen(str);
-    for(int i=(len-1);i>=0;i--)
+    int len=strlen(str);  //使用stelen函数获取长度
+
+    for(int i=(len-1);i>=0;i--)  //注意loop循环只在c99之后的标准支持,另外数组是从0开始
     {
         if(str[i]!=' ')
         {
-            count++;
+            count++;   //没有发现空格之前统计字符串长度
         }
         else
         {
